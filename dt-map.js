b811184ee8cc1e81e02376cf5bce63ff3242039d
Alireza Molaee <alirezamolaii@gmail.com>
2016-12-15 17:35:27 +0330

    add site

--- dt-map.js
+++ dt-map.js
@@ -1,6 +1,14 @@
 var dtaMap = (function() {
     function dtaMap(container, option) {
         option = option != null ? option : {};
+        option.sites = option.sites != null ? option.sites : {};
+
+        this.sites = {
+            cells: {},
+            name: {},
+            data: null
+        };
+
         this.extent = option.extent != null ? option.extent : {
             "min_lat": "33.85581940594627",
             "max_long": "55.73730647563934",
@@ -8,11 +16,15 @@ var dtaMap = (function() {
             "max_lat": "37.21633142738797"
         };
         this.container = container;
+        this.sites.opacity = option.sites.opacity != null ? option.sites.opacity : 0.8;
+        this.sites.radius = option.sites.radius != null ? option.sites.radius : 50;
+        this.sites.fill = option.sites.fill != null ? option.sites.fill : "colorful";
         this.zoom = option.zoom != null ? option.zoom : 10;
         this.googleMapOptions = option.googleMapOptions != null ? option.googleMapOptions : {};
 
         this.initGoogleMap();
         this.initOpenLayer();
+        this.initSites();
 
     }
 
@@ -62,6 +74,10 @@ var dtaMap = (function() {
             view: this.openlayer.view
         });
 
+        this.openlayer.map.on('moveend', function () {
+            $('#dtaMap-openLayerMap').css("opacity", "1");
+        });
+
         openlayerDiv.parentNode.removeChild(openlayerDiv);
 
         this.googleMapObject.controls[google.maps.ControlPosition.TOP_LEFT].push(openlayerDiv);
@@ -70,12 +86,112 @@ var dtaMap = (function() {
     };
 
     dtaMap.prototype.initSites = function () {
-        this.sites = {};
-        this.sites.olSource = new ol.source.Vector();
-        this.sites.olLayer = new ol.layer.Vector({
-            source: this.sites.olSource,
-            name: 'sites'
+        var radius = this.sites.radius;
+        this.sites.cells.olSource = new ol.source.Vector();
+        this.sites.cells.olLayer = new ol.layer.Vector({
+            source: this.sites.cells.olSource,
+            name: 'sites cell',
+            opacity: this.sites.opacity
         });
+        this.sites.cells.olLayer.setStyle(this.getSiteStyleFunction(this.sites.fill));
+
+        this.sites.name.olSource = new ol.source.Vector();
+        this.sites.name.olLayer = new ol.layer.Vector({
+            source: this.sites.name.olSource,
+            name: 'sites name',
+            opacity: this.sites.opacity,
+            style: function (feature, resolution) {
+                var offset = radius * 2;
+                if (resolution > 2.4) {
+                    offset = radius * 1.5 * 2
+                }
+                return new ol.style.Style({
+                    fill: null,
+                    stroke: null,
+                    text: new ol.style.Text({
+                        font: '11px sans-serif',
+                        text: feature.get('name'),
+                        offsetY: -(offset + 30 ) / resolution,
+                        fill: new ol.style.Fill({color: '#000'}),
+                        stroke: new ol.style.Stroke({
+                            color: "#fff",
+                            width: 1
+                        })
+                    })
+                })
+            }
+        });
+
+        this.openlayer.map.addLayer(this.sites.cells.olLayer);
+        this.openlayer.map.addLayer(this.sites.name.olLayer);
+    };
+
+    dtaMap.prototype.getSiteStyleFunction = function (type) {
+        var radius, getSieteStyle, siteStyleFunction;
+
+        radius = this.sites.radius;
+
+        getSieteStyle = function (feature, resolution) {
+            var offset, azimuthDgre, azimuthRadian, offsetx, offsety, fill, stork, text;
+            offset = radius;
+            if (resolution > 2.4) {
+                offset = radius * 1.5
+            }
+
+            azimuthDgre = (360 - (feature.get('start') + (feature.get('end') - feature.get('start')) / 2)) + 90;
+            azimuthRadian = (azimuthDgre * Math.PI) / 180;
+            offsetx = (Math.cos(azimuthRadian) * (offset + 10)) / resolution;
+            offsety = -(Math.sin(azimuthRadian) * (offset + 10)) / resolution;
+
+            fill = new ol.style.Fill({
+                color: feature.get('color')
+            });
+
+            stork = new ol.style.Stroke({
+                color: "#000",
+                width: 1
+            });
+
+            text = new ol.style.Text({
+                font: '11px sans-serif',
+                text: feature.get('cellName'),
+                offsetX: offsetx,
+                offsetY: offsety,
+                fill: new ol.style.Fill({color: '#000'}),
+                stroke: new ol.style.Stroke({
+                    color: "#fff",
+                    width: 2
+                })
+            });
+
+            return [fill, stork, text]
+        };
+
+        if (type === 'colorful') {
+            siteStyleFunction = function (feature, resolution) {
+                var styles = getSieteStyle(feature, resolution);
+                return new ol.style.Style({
+                    fill: styles[0],
+                    stroke: styles[1],
+                    text: styles[2]
+                })
+            }
+        } else {
+            siteStyleFunction = function (feature, resolution) {
+                var styles = getSieteStyle(feature, resolution);
+                return new ol.style.Style({
+                    fill: null,
+                    stroke: styles[1],
+                    text: styles[2]
+                })
+            }
+        }
+
+        return siteStyleFunction
+    };
+
+    dtaMap.prototype.changeSitesFill = function (type) {
+        this.sites.cells.olLayer.setStyle(this.getSiteStyleFunction(type))
     };
 
     dtaMap.prototype.changeMapType = function (type) {
@@ -121,10 +237,140 @@ var dtaMap = (function() {
     };
 
     dtaMap.prototype.addSites = function (sites) {
+        var radiusMeters = this.sites.radius;
+        this.sites.data = sites;
 
+        function calcStartEnd(cell) {
+            var start, end;
+            end = parseInt(cell.azimuth) + parseInt(cell.beamwidth / 2);
+            start = parseInt(cell.azimuth) - parseInt(cell.beamwidth / 2);
+            if (start > 360) {
+                start %= 360
+            }
+            if (end > 360) {
+                end %= 360
+            }
+            cell.start = start;
+            cell.end = end;
+        }
+
+        function makeCellGeometry(cell, centerLat, centerLong) {
+            var center, angle, points, point, centerPoint;
+            points = [];
+            center =  new google.maps.LatLng(centerLat, centerLong);
+            angle = cell.start;
+            while (angle !== cell.end) {
+                point = google.maps.geometry.spherical.computeOffset(center, radiusMeters, angle);
+                points.push(ol.proj.fromLonLat([point.toJSON().lng, point.toJSON().lat]));
+                angle += 1;
+                if (angle > 360) {
+                    angle = 1;
+                }
+            }
+            centerPoint = ol.proj.fromLonLat([center.toJSON().lng, center.toJSON().lat]);
+            points.unshift(centerPoint);
+            points.push(centerPoint);
+            return points
+        }
+
+        function makeCellFeature(cell, centerLat, centerLong) {
+            var cellGeometry, feature;
+            cellGeometry = makeCellGeometry(cell, centerLat, centerLong);
+
+            feature = new ol.Feature({
+                geometry: new ol.geom.Polygon([cellGeometry]),
+                cellId: cell.cell_id,
+                cellName: cell.cell_name,
+                color: cell.color,
+                start: cell.start,
+                end: cell.end,
+                azimuth: cell.azimuth,
+                beamwidth: cell.beamwidth
+            });
+
+            feature.setId(Number(cell.cell_id));
+
+            return feature;
+        }
+
+        sites.forEach(function (site) {
+            var labelFeature;
+            site.cells.forEach(function (cell) {
+                var feature;
+                calcStartEnd(cell);
+                feature = makeCellFeature(cell, site.lat, site.long);
+                this.sites.cells.olSource.addFeature(feature);
+            }, this);
+
+            labelFeature = new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.fromLonLat([Number(site.long), Number(site.lat)])),
+                labelPoint: new ol.geom.Point(ol.proj.fromLonLat([Number(site.long), Number(site.lat)])),
+                name: site.site_name
+            });
+            this.sites.name.olSource.addFeature(labelFeature);
+        }, this);
     };
 
+    dtaMap.prototype.clearSites = function () {
+        this.sites.cells.olSource.clear();
+        this.sites.name.olSource.clear();
+    };
 
+    dtaMap.prototype.changeSitesRadius = function (radius) {
+        this.sites.radius = radius;
+        this.clearSites();
+        this.addSites(this.sites.data);
+    };
+
+    dtaMap.prototype.getSiteByName = function (name) {
+        var filteredData;
+
+        function checkName(site) {
+            return site.site_name.includes(name);
+        }
+
+        filteredData = this.sites.data.filter(checkName);
+
+
+
+        filteredData.forEach(function (site) {
+            site.cells.forEach(function (cell) {
+                cell = this.getCellById(cell.cell_id);
+            }, this);
+        }, this);
+
+        if (filteredData.length === 1) {
+            return filteredData[0]
+        } else if (filteredData.length > 1) {
+            return filteredData
+        } else {
+            return null
+        }
+    };
+
+    dtaMap.prototype.getAllSite = function () {
+      return this.getSiteByName('');
+    };
+
+    dtaMap.prototype.getCellById = function(id) {
+        var cell;
+        cell = this.sites.cells.olSource.getFeatureById(Number(id)).getProperties();
+        delete cell.geometry;
+        return cell
+    };
+
+    dtaMap.prototype.getAllcell = function () {
+        var cellsFeature, cells;
+        cells = [];
+        cellsFeature = this.sites.cells.olSource.getFeatures();
+        cellsFeature.forEach(function (feature) {
+            var cell;
+            cell = feature.getProperties();
+            delete cell.geometry;
+            cells.push(cell);
+        });
+        return cells;
+    };
 
     return dtaMap
 })();
